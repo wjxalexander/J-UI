fb6a42cf9d3e7c271f3b2ce70d5f6bf7b729779a
wjxalexander <wjxalexander@hotmail.com>
2018-12-10 22:25:30 +0800

    new carsol deploy

--- docs/.vuepress/components/carousel-demo.vue
+++ docs/.vuepress/components/carousel-demo.vue
@@ -1,37 +1,33 @@
 <template>
-   <div class="carousel">
-      <j-carousel :selectdot = 1>
-        <template slot='content'>
-          <j-carouselitem :dataid=1>
-            <h3>1</h3>
-          </j-carouselitem>
-          <j-carouselitem :dataid=2>
-            <h3>2</h3>
-          </j-carouselitem>
-          <j-carouselitem :dataid=3>
-            <h3>3</h3>
-          </j-carouselitem>
-          <j-carouselitem :dataid=4>
-            <h3>4</h3>
-          </j-carouselitem>
-        </template>
-        <template slot="ctr">
-          <j-carouselcontrol :dataid=1></j-carouselcontrol>
-          <j-carouselcontrol :dataid=2></j-carouselcontrol>
-          <j-carouselcontrol :dataid=3></j-carouselcontrol>
-          <j-carouselcontrol :dataid=4></j-carouselcontrol>
-        </template>
-      </j-carousel>
+  <div class="carousel">
+    <j-carousel :selectdot="1">
+      <template slot="content">
+        <j-carouselitem :dataid="1">
+          <h3>1</h3>
+        </j-carouselitem>
+        <j-carouselitem :dataid="2">
+          <h3>2</h3>
+        </j-carouselitem>
+        <j-carouselitem :dataid="3">
+          <h3>3</h3>
+        </j-carouselitem>
+        <j-carouselitem :dataid="4">
+          <h3>4</h3>
+        </j-carouselitem>
+      </template>
+      <template slot="ctr">
+        <j-carouselcontrol :totalnum="4"></j-carouselcontrol>
+      </template>
+    </j-carousel>
     <div class="collapsedemo">
       <j-collapse>
-        <template slot='title' > 查看代码 </template>
-        <template slot='body'> 
+        <template slot="title">查看代码</template>
+        <template slot="body">
           <pre><code>{{this.code}}</code></pre>
         </template>
       </j-collapse>
     </div>
-    </div>
-
+  </div>
 </template>
 <script>
 import Collapse from "../../../src/collapse";
@@ -45,9 +41,9 @@ export default {
     "j-carouselitem": Carouselitem,
     "j-carouselcontrol": Carouselcontrol
   },
-   data(){
-      return {
-        code:`<div class="carousel">
+  data() {
+    return {
+      code: `<div class="carousel">
       <j-carousel :selectdot = 1>
         <template slot='content'>
           <j-carouselitem :dataid=1>
@@ -63,17 +59,16 @@ export default {
             <h3>4</h3>
           </j-carouselitem>
         </template>
+        
         <template slot="ctr">
-          <j-carouselcontrol :dataid=1></j-carouselcontrol>
-          <j-carouselcontrol :dataid=2></j-carouselcontrol>
-          <j-carouselcontrol :dataid=3></j-carouselcontrol>
-          <j-carouselcontrol :dataid=4></j-carouselcontrol>
+          <j-carouselcontrol :totalnum=4></j-carouselcontrol>
         </template>
       </j-carousel>
-    </div>`.replace(/\t+| |/g,'').trim()
-      }
-
-   }
+    </div>`
+        .replace(/\t+| |/g, "")
+        .trim()
+    };
+  }
 };
 </script>
 <style lang="scss" scoped>
